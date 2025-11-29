f5851fc9534805c5f8b1c1a5913ad9a8f9dff210
Matheus <matheustonial97@gmail.com>
2025-11-29 13:52:24 -0300

    feat: Populate ListView with static data
    Populates the `ListView` in `ListarActivity` with a static list of strings using an `ArrayAdapter`.
    
    This includes:
    - A new `initList` private function to encapsulate the logic.
    - Creation of a hardcoded list of strings.
    - Instantiation of an `ArrayAdapter` to bind the list to the `ListView`.
    - Applying window insets to the `lvRegistros` `ListView` instead of the root `main` view.

--- app/src/main/java/com/tonial/usandosqlite/ListarActivity.kt
+++ app/src/main/java/com/tonial/usandosqlite/ListarActivity.kt
@@ -1,6 +1,7 @@
 package com.tonial.usandosqlite
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +19,20 @@ class ListarActivity : AppCompatActivity() {
         binding = ActivityListarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lvRegistros)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initList()
+    }
+
+    private fun initList() {
+        val lista = listOf<String>("Brasil", "Argentina", "Chile", "Uruguai")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista)
+
+        binding.lvRegistros.adapter = adapter
     }
 }
